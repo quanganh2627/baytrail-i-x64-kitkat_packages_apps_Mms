c3f46336841e8928c0f9d50a43f0e2b9a3207593
Blackbay Build <blackbay-build@intel.com>
2011-09-28 03:25:43 -0700

    [PORT FROM MAIN] Messaging: Bug 7844 - Send Messaging with attachment,repeat 10-20 times PR2 Crash.
    BZ: 7844
    
    Borqs-Id: 0dc17f87711da2f1aec5bdbbc86c6ff9971e587a
    Borqs-Contact: b129 <jianli.zhang@borqs.com>
    Commit-Date: Wed Sep 28 03:25:05 PDT 2011
    
    Orig-Change-Id: I6ae8c77d93058d1696d86ccf92416e695e12bf4f
    Category: aosp improvement
    Domain: AOSP-Messaging
    Origin: internal
    Upstream-Candidate: yes
    Change-Id: Ib46d7ebf34353dc1cc21475ae90bdb585ef04ba4
    borqsbtx: 5495

--- src/com/android/mms/data/WorkingMessage.java
+++ src/com/android/mms/data/WorkingMessage.java
@@ -1581,6 +1581,9 @@ public class WorkingMessage {
             return res;
         } catch (MmsException e) {
             return null;
+        } catch (IllegalStateException e) {
+            Log.e(TAG,"failed to create draft mms "+ e);
+            return null;
         }
     }
 
