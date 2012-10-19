94be46dcbf2cc5fd0469ab14491a23ab5c56c46b
Jun Wu <junx.wu@intel.com>
2012-10-20 00:37:27 +0800

    mms: fix crash when click mms by BT mouse right button.
    BZ: 55573
    
    Currently, info is not checked before extracting
    position from info resulting in NullPointerException.
    
    Check the info before extracting position.
    
    Change-Id: I4cab3b05846e74b05724670afa4cfa50c94f9d50
    Signed-off-by: Jun Wu <junx.wu@intel.com>
    Reviewed-on: http://android.intel.com:8080/76481
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -901,6 +901,9 @@ public class ComposeMessageActivity extends Activity
             Log.e(TAG, "bad menuInfo");
             return;
         }
+        if (info == null) {
+           return;
+        }
         final int position = info.position;
 
         addUriSpecificMenuItems(menu, v, position);
