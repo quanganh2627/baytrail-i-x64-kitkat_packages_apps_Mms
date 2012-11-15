0e2e0eca059e2799a4649788dd0541f91dd699a4
Bin Yang <bin.y.yang@intel.com>
2012-11-15 18:03:30 +0800

    [PORT FROM MAIN]MMS: The audio should keep playing when pressing the menu button
    BZ: 73900
    
    The function of the menu key is incorrect when browsing MMS in messaging
    Remove the MENU KEY support
    
    Category: aosp improvement
    Domain: AOSP-Messaging
    Origin: internal
    Upstream-Candidate: yes
    
    Orig-Change-Id: I9a4a8dab14d7aaa3c112b92a19a73ceeddf98627
    Change-Id: I871e515d5fabc2216a11d29ee3098556866bd82d
    Signed-off-by: Bin Yang <bin.y.yang@intel.com>

--- src/com/android/mms/ui/SlideshowActivity.java
+++ src/com/android/mms/ui/SlideshowActivity.java
@@ -318,7 +318,6 @@ public class SlideshowActivity extends Activity implements EventListener {
             case KeyEvent.KEYCODE_DPAD_RIGHT:
                 break;
             case KeyEvent.KEYCODE_BACK:
-            case KeyEvent.KEYCODE_MENU:
                 if ((mSmilPlayer != null) &&
                         (mSmilPlayer.isPausedState()
                         || mSmilPlayer.isPlayingState()
