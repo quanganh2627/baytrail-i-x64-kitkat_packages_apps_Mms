quanganh2627/baytrail-i-x64-kitkat_packages_apps_Mms

commit 92d5b5d084a1acc7145a502571f0bd3fe59b6697

[PORT FROM MAIN] Mms: Process has stopped after dragging progress
BZ: 28634

testcase 1> create a slideshow mms and play it. Tap progress bar
                continuously until play finished.
         2> create a slideshow mms and play it. Tap progress bar
                continuously when rotate the screen

We have to set the seek bar change listener to null,otherwise if
the activity stop during tap progress bar continuously, window
will leak.

Borqs-Id: eeadc0f138cd34c8ffd8807a160cbafb555aef27
Borqs-Contact: b533 <b533@borqs.com>

Change-Id: I131f237f2e55da408256e81d239638b66f32c6bd
Signed-off-by: Frederic Predon <frederic.predon@intel.com>
Reviewed-on: http://android.intel.com:8080/75036
Reviewed-by: Badrappan, Jeevaka <jeevaka.badrappan@intel.com>
Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
Reviewed-by: cactus <cactus@intel.com>
Tested-by: cactus <cactus@intel.com>

src/com/android/mms/ui/SlideshowActivity.java
@@ -42,6 +42,7 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.MediaController;
 import android.widget.MediaController.MediaPlayerControl;
+import android.widget.SeekBar;
 
 import com.android.mms.R;
 import com.android.mms.dom.AttrImpl;
@@ -284,10 +285,23 @@ public class SlideshowActivity extends Activity implements EventListener {
                 mSmilPlayer.stopWhenReload();
             }
             if (mMediaController != null) {
+                // Must set the seek bar change listener null,otherwise if we rotate it
+                // during tap progress bar continuously,window will leak
+                SeekBar seeker = (SeekBar) mMediaController
+                        .findViewById(com.android.internal.R.id.mediacontroller_progress);
+                seeker.setOnSeekBarChangeListener(null);
                 // Must do this so we don't leak a window.
                 mMediaController.hide();
             }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mSlideView != null) {
+            mSlideView.setMediaController(null);
+        }
+        super.onDestroy();
     }
 
     @Override

commit 57f240a71522ecce5951e4e67b3ed9a70cc15102

Mms: Fix Null pointer dereference error
BZ: 80783

This patch fixes the Null Pointer dereference
error introduced by commit
92d5b5d084a1acc7145a502571f0bd3fe59b6697

Change-Id: Ib1c03e2d04a611f98e5a83a9aae15ae14f2dd855
Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
Reviewed-on: http://android.intel.com:8080/90122
Reviewed-by: cactus <cactus@intel.com>
Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
Reviewed-by: Yao, Yong <yong.yao@intel.com>
Reviewed-by: Lucas, Guillaume <guillaume.lucas@intel.com>
Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
Reviewed-by: buildbot <buildbot@intel.com>
Tested-by: buildbot <buildbot@intel.com>

src/com/android/mms/ui/SlideshowActivity.java
@@ -293,7 +293,10 @@ public class SlideshowActivity extends Activity implements EventListener {
                 // during tap progress bar continuously,window will leak
                 SeekBar seeker = (SeekBar) mMediaController
                         .findViewById(com.android.internal.R.id.mediacontroller_progress);
-                seeker.setOnSeekBarChangeListener(null);
+                if (seeker != null) {
+                    seeker.setOnSeekBarChangeListener(null);
+                }
+
                 // Must do this so we don't leak a window.
                 mMediaController.hide();
             }